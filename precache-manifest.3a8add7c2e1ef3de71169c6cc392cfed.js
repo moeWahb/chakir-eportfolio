d2f5d0a317a881f8303ab7c7e3240e3b6ffffa1c
moeWahb <moewahb21@gmail.com>
2022-12-10 00:08:44 +0100

    Updates

---
+++ precache-manifest.3a8add7c2e1ef3de71169c6cc392cfed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6658aa8d1089945eb42c87431bdddeed",
+    "url": "/chakir-eportfolio/index.html"
+  },
+  {
+    "revision": "dbc003a7c40ac6e597ae",
+    "url": "/chakir-eportfolio/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "be0c9da7d13b7f6f1d21",
+    "url": "/chakir-eportfolio/static/js/2.6378059b.chunk.js"
+  },
+  {
+    "revision": "dbc003a7c40ac6e597ae",
+    "url": "/chakir-eportfolio/static/js/main.ed0bd9a2.chunk.js"
+  },
+  {
+    "revision": "100c936b0aa1e1dd60c4",
+    "url": "/chakir-eportfolio/static/js/runtime~main.5610e463.js"
+  },
+  {
+    "revision": "79bd0e506f983b5fe907e2b6e82b0d4b",
+    "url": "/chakir-eportfolio/static/media/profile.79bd0e50.jpg"
+  }
+]);
